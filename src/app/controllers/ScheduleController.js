4d45558f437964af6ed9158a01eaa6972790b8f7
Eduardo Viana <eduardocosta_viana@live.com>
2019-10-21 08:26:37 -0300

    First Commit

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,49 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date, page = 1 } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: { [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)] },
+      },
+      attributes: ['user_id', 'date'],
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://131248f023064f4a9abbfd89848b4381@sentry.io/1785194',
+};
